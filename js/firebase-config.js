57b8eeb5839de315230a99bce395ef8d33b6ac31
potex05 <potetopoteto05@gmail.com>
2025-06-16 16:22:09 +0900

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBeq08o2hPiM8RQmvVrei8cCCqtVfijiGs",
+  authDomain: "selforder-system-01.firebaseapp.com",
+  databaseURL: "https://selforder-system-01-default-rtdb.firebaseio.com",
+  projectId: "selforder-system-01",
+  storageBucket: "selforder-system-01.firebasestorage.app",
+  messagingSenderId: "517977276669",
+  appId: "1:517977276669:web:b44e39f4446947dab27e94",
+  measurementId: "G-BZLNHSWMY8"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
